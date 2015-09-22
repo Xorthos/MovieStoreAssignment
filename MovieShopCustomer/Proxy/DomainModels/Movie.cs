be928869909ce104c1edb21bbf5305ff1c36d8a6
Xorthos <github@minimads.com>
2015-09-22 14:41:24 +0200

    Administration project added

--- MovieShopCustomer/Proxy/DomainModels/Movie.cs
+++ MovieShopCustomer/Proxy/DomainModels/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace Proxy.DomainModels
 {
     public class Movie
     {
+        [Key]
         public int ID { get; set; }
         public string Title { get; set; }
         public double price { get; set; }
