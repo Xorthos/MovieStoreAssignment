e185d7d3630b7e3d5fd16fcbf84562fad0800d6b
MaxHansen <v.6@live.dk>
2015-10-02 11:41:51 +0200

    I made stuff

---
+++ MovieShopCustomer/MovieShopCustomer/Controllers/CustomerController.cs
@@ -0,0 +1,35 @@
+﻿using Proxy.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieShopCustomer.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Creates a new customer.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult NewCustomer()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult NewCustomer(Customer cust)
+        {
+            return Redirect("Index");
+        }
+    }
+}
--- MovieShopCustomer/Proxy/DomainModels/Customer.cs
+++ MovieShopCustomer/Proxy/DomainModels/Customer.cs
@@ -12,16 +12,22 @@ namespace Proxy.DomainModels
         [Key]
         public int Id { get; set; }
         [Required]
+        [DataType("Text")]
         public string FirstName { get; set; }
+        [DataType("Text")]
         public string MiddleName { get; set; }
         [Required]
+        [DataType("Text")]
         public string LastName { get; set; }
         [Required]
+        [DataType("Text")]
         public string StreetName { get; set; }
         [Required]
+        [DataType("Number")]
         public int StreetNumber { get; set; }
         [Required]
         [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}")]
+        [DataType("Text")]
         public string Email { get; set; }
 
         /// <summary>
